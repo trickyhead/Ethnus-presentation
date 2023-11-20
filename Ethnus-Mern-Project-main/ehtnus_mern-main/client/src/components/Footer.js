aff2f0d11e0abb523c98b2c54b8cde7e441c9581
Vijwal Mahendrakar <57533973+trickyhead@users.noreply.github.com>
2023-11-20 14:18:38 +0530

    Add files via upload

---
+++ Ethnus-Mern-Project-main/ehtnus_mern-main/client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Card, Grid, Link, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import Copyright from "./Copyright";
+
+const Footer = () => {
+  return (
+    <Box pb={3}>
+      <Card>
+        <Typography variant="subtitle1">
+          Thank You For Watching!!!
+        </Typography>
+      </Card>
+    </Box>
+  );
+};
+
+export default Footer;
